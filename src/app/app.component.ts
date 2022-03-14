ccd879c8fcd729f944373b64baceef4a152c3c28
Carmelo Ienco <c.ienco@xeel.tech>
2022-03-14 16:46:43 +0100

    modifiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   authConfig: AuthConfig = {
     issuer: 'http://localhost:8080/auth/realms/princnr',
     redirectUri: window.location.origin + "/users",
-    clientId: 'princnr-fe',
+    clientId: 'projectx-fe',
     //dummyClientSecret: '2ea5dc66-8e1c-4e7c-aac0-52a42594a6ac',
     scope: 'openid profile email offline_access users',
     responseType: 'code',
@@ -38,6 +38,47 @@ export class AppComponent {
   private configure() {
     this.oauthService.configure(this.authConfig);
     this.oauthService.tokenValidationHandler = new NullValidationHandler();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.loadDiscoveryDocumentAndLogin().then(
+      (isLoggedIn) => {
+
+        if (isLoggedIn) {
+          this.oauthService.setupAutomaticSilentRefresh();
+          // resolveFn();
+        } else {
+          this.oauthService.initImplicitFlow();
+          // rejectFn();
+        }
+      },
+      (error) => {
+        console.log({ error });
+        if (error.status === 400) {
+          location.reload();
+        }
+      }
+    );
+    // this.oauthService.events.subscribe(_ => {
+    //   this.isAuthenticatedSubject$.next(this.oauthService.hasValidAccessToken());
+    // });
+    // this.oauthService.events.subscribe(
+    //   hasReceivedTokens => {
+    // if (hasReceivedTokens instanceof token_received ) {
+    //     // this would have stored all the tokens needed
+    //     if (hasReceivedTokens) {
+    //       // carry on with your app
+    //       return Promise.resolve();
+    //
+    //       /* if you wish to do something when the user receives tokens from the identity server,
+    //        * use the event stream or the `onTokenReceived` callback in LoginOptions.
+    //        *
+    //        * this.oauthService.events(filter(e => e.type === 'token_received')).subscribe()
+    //        */
+    //     } else {
+    //       // may want to check if you were previously authenticated
+    //       if (this.oauthService.hasValidAccessToken() && this.oauthService.hasValidIdToken()) {
+    //         return Promise.resolve();
+    //       }
+    //     }
+    //   }
+    // });
   }
 }
