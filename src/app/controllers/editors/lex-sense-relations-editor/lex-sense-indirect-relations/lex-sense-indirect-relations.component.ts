1743f563f06888966a781be45a3f7771fb8881f4
Enrico Carniani <6718038+kicco73@users.noreply.github.com>
2023-11-29 11:13:21 +0100

    first refactoring

---
+++ src/app/controllers/editors/lex-sense-relations-editor/lex-sense-indirect-relations/lex-sense-indirect-relations.component.ts
@@ -0,0 +1,20 @@
+import { Component, SimpleChanges } from '@angular/core';
+import { BaseLexEntityRelationsComponent } from '../../lex-entry-relations-editor/base-lex-entity-relations/base-lex-entity-relations.component';
+import { LexSenseIndirectRelationsStrategy } from './lex-sense-indirect-relations-strategy';
+
+@Component({
+  selector: 'app-lex-sense-indirect-relations',
+  templateUrl: './lex-sense-indirect-relations.component.html',
+  styleUrls: ['./lex-sense-indirect-relations.component.scss']
+})
+
+export class LexSenseIndirectRelationsComponent extends BaseLexEntityRelationsComponent  {
+
+  override ngOnChanges(changes: SimpleChanges): void {
+    if (changes['lexEntityId']) {
+      this.strategy = new LexSenseIndirectRelationsStrategy(this.lexiconService, this.lexEntityId);
+    }
+    super.ngOnChanges(changes);
+  }
+
+}
