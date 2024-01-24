f7f5fa9e37fbe6b9f065e098ee6d7ed973311698
Mafalda <papini.mafalda@gmail.com>
2024-01-24 12:49:26 +0100

    cleanup tagset.service updated documentation

--- src/app/services/tagset.service.ts
+++ src/app/services/tagset.service.ts
@@ -4,15 +4,12 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TTagset } from '../models/texto/t-tagset';
 import { TTagsetItem } from '../models/texto/t-tagset-item';
-import { Tagset } from './../models/tagset/tagset';
 
 /**Tagset-related services */
 @Injectable({
   providedIn: 'root'
 })
 export class TagsetService {
-  /**Url for tagset-related requests */
-  private tagsetUrl: string;
   /**Url for texto-related requests */
   private textoUrl: string;
 
@@ -21,79 +18,9 @@ export class TagsetService {
    * @param http {HttpClient} Performs HTTP requests
    */
   constructor(private http: HttpClient) {
-    this.tagsetUrl = environment.tagsetUrl; //recupero l'url dall'environment
     this.textoUrl = environment.maiaBeTextoUrl;
   }
 
-  /**
-   * GET che recupera l'elenco completo dei tagset
-   * @returns {Observable<Tagset[]>} observable della lista di tagset
-   */
-  public retrieve(): Observable<Tagset[]> { //TODO CHECK TO REMOVE @MPapini91
-    // MOCK
-    // return this.http.get<Tagset[]>(`assets/mock/tagsets.json`);
-    // FINE MOCK
-
-    /* La seguente riga è da DECOMMENTARE dopo aver demockato */
-    return this.http.get<Tagset[]>(this.tagsetUrl);
-  }
-
-  /**
-   * GET che recupera un dato tagset attraverso l'id
-   * @param id {number} identificativo numerico del tagset
-   * @returns {Observable<Tagset>} observable del tagset recuperato
-   */
-  public retrieveById(id: number): Observable<Tagset> { //TODO CHECK TO REMOVE @MPapini91
-    // MOCK
-    //return this.http.get<Tagset>(`assets/mock/tagsets/tagset${id}.json`);
-    // FINE MOCK
-
-    /* La seguente riga è da DECOMMENTARE dopo aver demockato */
-    return this.http.get<Tagset>(`${this.tagsetUrl}/${id}`);
-  }
-
-  /**
-   * POST che richiede la creazione di un nuovo tagset
-   * @param item {Tagset} tagset da inserire
-   * @returns {Observable<Tagset>} observable del nuovo tagset
-   */
-  public create(item: Tagset) { //TODO CHECK TO REMOVE @MPapini91
-    // MOCK
-    //return this.http.post<Tagset>('assets/mock/tagsets.json', item);
-    // FINE MOCK
-
-    /* La seguente riga è da DECOMMENTARE dopo aver demockato */
-    return this.http.post<Tagset>(`${this.tagsetUrl}`, item);
-  }
-
-  /**
-   * PUT che aggiorna i dati del tagset
-   * @param item {Tagset} tagset modificato
-   * @returns {Observable<Tagset>} observable del tagset modificato
-   */
-  public update(item: Tagset): Observable<Tagset> { //TODO CHECK TO REMOVE @MPapini91
-    return this.http.put<Tagset>(`${this.tagsetUrl}`, item);
-  }
-
-  /**
-   * DELETE che richiede la rimozione di un tagset
-   * @param id {number} identificativo numerico del tagset
-   * @returns {Observable<boolean>} observable dell'esito della rimozione
-   */
-  public delete(id: number): Observable<boolean> {  //TODO CHECK TO REMOVE @MPapini91
-    return this.http.delete<boolean>(`${this.tagsetUrl}/${id}`);
-  }
-
-  /**
-   * GET che richiede se un tagset è cancellabile
-   * @param id {number} identificativo numerico del tagset
-   * @returns {Observable<boolean>} observable che definisce se un tagset può essere cancellato o meno
-   */
-  public retrieveCanBeDeleted(id: number): Observable<boolean> {  //TODO CHECK TO REMOVE @MPapini91
-    return this.http.get<boolean>(`${this.tagsetUrl}/canbedeleted/${id}`);
-  }
-
-  //#region TEXTO
   /**
    * POST to add a new tagset
    * @param newTagset {TTagset} new tagset to be added
@@ -177,5 +104,4 @@ export class TagsetService {
       updatedTagsetItem,
     );
   }
-  //#endregion
 }
