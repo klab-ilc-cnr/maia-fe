16d071ebe5a6637a9f2d0da45e726910a4a3becc
Mafalda <papini.mafalda@gmail.com>
2024-02-06 09:19:58 +0100

    Added interface for system information Added basic table

--- src/app/pages/system-information/system-information.component.ts
+++ src/app/pages/system-information/system-information.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SystemInformation } from 'src/app/models/system-information';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-system-information',
@@ -6,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./system-information.component.scss']
 })
 export class SystemInformationComponent implements OnInit {
+  private interfaceVersion = environment.interfaceVersion;
+  private angularVersion = environment.angularVersion;
+  versionsList: SystemInformation[] = [
+    <SystemInformation>{
+      name: 'maia-fe',
+      version: this.interfaceVersion.toString(),
+      serverTime: '',
+      javaVersion: ''
+    },
+    <SystemInformation>{
+      name: 'Angular',
+      version: this.angularVersion.toString(),
+      serverTime: '',
+      javaVersion: ''
+    }
+  ];
 
   constructor() { }
 
