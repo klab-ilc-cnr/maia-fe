6849aa0101733238c494ca8e66ecf174ae08fbc3
Carmelo Ienco <c.ienco@xeel.tech>
2024-03-06 12:33:36 +0100

    #27 fix index on lateral windows opens

--- src/app/pages/workspace/workspace-text-window/workspace-text-window.component.ts
+++ src/app/pages/workspace/workspace-text-window/workspace-text-window.component.ts
@@ -1220,7 +1220,7 @@ export class WorkspaceTextWindowComponent implements OnInit, OnDestroy {
       case ScrollingDirectionType.InRange:
         if (!this.currentVisibleRow) { break; }
 
-        scrolledBlockSize = this.rows.filter(r => r.rowIndex! < this.currentVisibleRow!.rowIndex - 1).reduce((acc, o) => acc + (o.height || 0), 0);
+        scrolledBlockSize = this.rows.filter(r => r.rowIndex! < this.currentVisibleRow!.rowIndex).reduce((acc, o) => acc + (o.height || 0), 0);
         break;
     }
 
@@ -1246,6 +1246,8 @@ export class WorkspaceTextWindowComponent implements OnInit, OnDestroy {
     const scrollHeightStart = this.textContainer.nativeElement.clientHeight + this.textContainer.nativeElement.scrollTop;
     const scrollHeigthEnd = scrollHeightStart + this.textContainer.nativeElement.clientHeight;
     let partialHeight = 0;
+
+    if (this.textContainer.nativeElement.scrollTop === 0) { return this.rows[0]; }
 
     for (var fieldIndex = 0; fieldIndex < this.rows.length; fieldIndex++) {
       let row = this.rows[fieldIndex];
