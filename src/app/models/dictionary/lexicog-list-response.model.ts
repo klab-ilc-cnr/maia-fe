e12b79e10f0633f11bdc45f56602bf8322c41812
Mafalda <papini.mafalda@gmail.com>
2024-04-09 16:13:57 +0200

    Draft integration of LexO service for dictionary entry retrieval

--- src/app/models/dictionary/lexicog-list-response.model.ts
+++
@@ -1,8 +0,0 @@
-import { LexicogEntryCore } from "./lexicog-entry-core.model";
-
-export interface LexicogListResponse {
-    /**Total number of results */
-    totalHits: number;
-    /**List of lexicog entries retrieved */
-    list: LexicogEntryCore[];
-}
--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -1,18 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LexicogListResponse } from '../models/dictionary/lexicog-list-response.model';
+import { environment } from 'src/environments/environment';
+import { LexicogEntriesResponse } from '../models/dictionary/lexicog-entries-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DictionaryService {
+  private lexoUrl: string;
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.lexoUrl = environment.maiaBeLexoUrl;
+  }
 
-  retrieveLexicogEntryList(): Observable<LexicogListResponse> {
-    return this.http.get<LexicogListResponse>('assets/mock/dictionary/lexicog-entries.json'); //FIXME replace with backend service
+  retrieveLexicogEntryList(): Observable<LexicogEntriesResponse> {
+    // return this.http.post<LexicogEntriesResponse>(
+    //   `${this.lexoUrl}/lexicon/data/dictionaryEntries`,
+    //   <LexicogEntriesRequest>{ //FIXME da sostituire con un body passato, serve solo per il test preliminare
+    //     text: '',
+    //     searchMode: searchModeEnum.startsWith,
+    //     pos: '',
+    //     author: '',
+    //     lang: '',
+    //     status: '',
+    //     offset: 0,
+    //     limit: 0
+    //   }
+    // );
+    return this.http.get<LexicogEntriesResponse>('assets/mock/dictionary/lexicog-entries.json'); //FIXME replace with backend service
   }
 }
