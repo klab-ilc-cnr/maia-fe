40b3db8c3d6308e14461f1385accd298717ea7e1
Mafalda <papini.mafalda@gmail.com>
2024-04-09 17:16:14 +0200

    Started development dialog "Create entry"

---
+++ src/app/pages/workspace/workspace-dictionary-tile/new-dictionary-entry/new-dictionary-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-dictionary-entry',
+  templateUrl: './new-dictionary-entry.component.html',
+  styleUrls: ['./new-dictionary-entry.component.scss']
+})
+export class NewDictionaryEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
