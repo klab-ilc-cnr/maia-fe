aa15540e9ff1a9ad2f2c40340395ba0393b16cf6
Mafalda Papini <papini.mafalda@gmail.com>
2024-04-11 17:39:17 +0200

    Ongoing development of the Create Voice dialog

--- src/app/pages/workspace/workspace-dictionary-tile/new-dictionary-entry/new-dictionary-entry.component.ts
+++ src/app/pages/workspace/workspace-dictionary-tile/new-dictionary-entry/new-dictionary-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { GlobalStateService } from 'src/app/services/global-state.service';
 
 @Component({
   selector: 'app-new-dictionary-entry',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-dictionary-entry.component.scss']
 })
 export class NewDictionaryEntryComponent implements OnInit {
+  languages$ = this.globalState.languages$;
 
-  constructor() { }
+  entryForm = new FormGroup({
+    language: new FormControl<string>('', [Validators.required]),
+    name: new FormControl<string>('', [Validators.required]),
+    lemmas: new FormArray([])
+  });
+  get lemmas() { return <FormArray>this.entryForm.get('lemmas') }
+
+  constructor(
+    private globalState: GlobalStateService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onAddLemma(isDefault?: boolean) {
+    const newLemma = { lemma: new FormControl<string>(''), pos: new FormControl<string>(''), type: new FormControl<string>('') };
+    this.lemmas.push(new FormGroup(newLemma));
+  }
+
+  onRemoveLemma(index: number) {
+    this.lemmas.removeAt(index);
+  }
 }
