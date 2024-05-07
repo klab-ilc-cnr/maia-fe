68a7ad96e4aa600f2ee181803f46edceea5a03f8
Mafalda Papini <papini.mafalda@gmail.com>
2024-05-07 18:13:30 +0200

    Added configurations for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,21 +43,19 @@ import { WorkspaceLayoutComponent } from './layouts/workspace-layout/workspace-l
 import { SharedModule } from './modules/shared.module';
 import { LoginComponent } from './pages/login/login.component';
 import { WorkspaceCorpusExplorerComponent } from './pages/workspace/workspace-corpus-explorer/workspace-corpus-explorer.component';
+import { NewDictionaryEntryComponent } from './pages/workspace/workspace-dictionary-tile/new-dictionary-entry/new-dictionary-entry.component';
+import { WorkspaceDictionaryTileComponent } from './pages/workspace/workspace-dictionary-tile/workspace-dictionary-tile.component';
 import { WorkspaceLexiconEditTileComponent } from './pages/workspace/workspace-lexicon-edit-tile/workspace-lexicon-edit-tile.component';
 import { WorkspaceLexiconTileComponent } from './pages/workspace/workspace-lexicon-tile/workspace-lexicon-tile.component';
 import { WorkspaceListComponent } from './pages/workspace/workspace-list/workspace-list.component';
 import { WorkspaceMenuComponent } from './pages/workspace/workspace-menu/workspace-menu.component';
+import { WorkspaceSearchTileComponent } from './pages/workspace/workspace-search-tile/workspace-search-tile.component';
 import { WorkspaceTextSelectorComponent } from './pages/workspace/workspace-text-selector/workspace-text-selector.component';
 import { WorkspaceTextWindowComponent } from './pages/workspace/workspace-text-window/workspace-text-window.component';
 import { WorkspaceComponent } from './pages/workspace/workspace.component';
 import { PendingChangesGuard } from './pending-changes-guard';
 import { CommonService } from './services/common.service';
 import { MatchNewPasswordDirective } from './validators/match-new-password.directive';
-import { WorkspaceSearchTileComponent } from './pages/workspace/workspace-search-tile/workspace-search-tile.component';
-import { CorpusStateService } from './services/corpus-state.service';
-import { SearchService } from './services/search.service';
-import { WorkspaceDictionaryTileComponent } from './pages/workspace/workspace-dictionary-tile/workspace-dictionary-tile.component';
-import { NewDictionaryEntryComponent } from './pages/workspace/workspace-dictionary-tile/new-dictionary-entry/new-dictionary-entry.component';
 
 @NgModule({
   declarations: [
@@ -130,5 +128,5 @@ export class AppModule {
  * @returns {TranslateHttpLoader}
  */
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
+  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
 }
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,67 +1,21 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-
-const clientid = "projectx-fe";
-//const appName = "/projectx-fe"
-const appName = "/maia-fe" //path del deploy
-//const issuer = "http://146.48.93.223:8081/realms/princnr"; //TODO sostituire con keycloak
-//const issuer = "http://localhost:8081/realms/princnr"; //TODO sostituire con keycloak
-const issuer = "https://klab.ilc.cnr.it/keycloak/realms/princnr"; //TODO sostituire con keycloak
-
-//const allowedUrls = 'http://146.48.93.223:9000/projectx/api'; //TODO sostituire con il backend xeel
-//const allowedUrls   = 'http://146.48.93.223:8080/projectx/api'; //TODO sostituire con il backend xeel
-//const allowedUrls   = 'http://localhost:8080/projectx/api'; //TODO sostituire con il backend xeel
-const allowedUrls = 'https://klab.ilc.cnr.it/projectx/api'; //TODO sostituire con il backend xeel
+const serverUrl = window.location.protocol + "//" + window.location.hostname + window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/")) + '/maia';
 
 export const environment = {
-  production: true,
-  keycloak: {
-    issuer: issuer,
-    redirectUri: window.location.origin + appName,
-    clientId: clientid,
-    scope: 'openid profile email offline_access',
-    responseType: 'code',
-    // logoutUrl: logoutUrl + clientid,
-    skipIssuerCheck: true,
-    // Remove the requirement of using Https to simplify the demo
-    // THIS SHOULD NOT BE USED IN PRODUCTION
-    // USE A CERTIFICATE FOR YOUR IDP
-    // IN PRODUCTION
-    requireHttps: false, //TODO verificare se passare a https per il deploy di prova
-    // at_hash is not present in JWT token
-    showDebugInformation: true,
-    //   // at_hash is not present in JWT token
-    disableAtHashCheck: true,
-    strictDiscoveryDocumentValidation: false //https://manfredsteyer.github.io/angular-oauth2-oidc/docs/additional-documentation/using-an-id-provider-that-fails-discovery-document-validation.html
-  },
-  allowedUrls: allowedUrls,
-  usersUrl: allowedUrls + '/users',
-  languagesUrl: allowedUrls + '/languages',
-  workspacesUrl: allowedUrls + '/workspaces',
-  layersUrl: allowedUrls + '/layers',
-  featureUrl: allowedUrls + '/features',
-  tagsetUrl: allowedUrls + '/tagsets',
-  annotationUrl: allowedUrls + '/annotations',
-  relationUrl: allowedUrls + '/relations',
-  //lexoUrl: "http://licodemo.ilc.cnr.it:8080/LexO-backend-maia/service/",
-  lexoUrl: "https://klab.ilc.cnr.it/LexO-backend-maia/service/",
-  //cashUrl: "http://146.48.93.223:8080/cash-0.0.1-SNAPSHOT", //TODO sostituire con il deploy di cash (o altro backend del testo)
-  cashUrl: "https://klab.ilc.cnr.it/cash-0.0.1-SNAPSHOT", //TODO sostituire con il deploy di cash (o altro backend del testo)
-  lexoPrefix: "ferrandi",
-  lexoBaseIRI: "http://rut/somali/ferrandi#",
-  textoUrl: "https://146.48.93.234:9443",
-  textoDebugUrl: "https://146.48.93.234:9443",
-  applicationSubTitle: " - Rut",
-  demoHide: true,
+  production: false,
+  envName: 'local',
+  maiaBeUrl: `${serverUrl}/api`,
+  usersUrl: `${serverUrl}/api/users`,
+  languagesUrl: `${serverUrl}/api/languages`,
+  workspacesUrl: `${serverUrl}/api/workspaces`,
+  layersUrl: `${serverUrl}/api/layers`,
+  featureUrl: `${serverUrl}/api/features`,
+  tagsetUrl: `${serverUrl}/api/tagsets`,
+  annotationUrl: `${serverUrl}/api/annotations`,
+  relationUrl: `${serverUrl}/api/relations`,
+  maiaBeLexoUrl: `${serverUrl}/lexo`,
+  maiaBeTextoUrl: `${serverUrl}/texto`,
+  lexoPrefix: "lex",
+  lexoBaseIRI: "http://lexica/mylexicon#",
+  applicationSubTitle: " VocaBO",
+  demoHide: false,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
