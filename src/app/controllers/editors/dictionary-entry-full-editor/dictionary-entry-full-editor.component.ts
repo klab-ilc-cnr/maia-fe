7070b954ceb18c589e28b62ae58aec5770b09e01
Mafalda <papini.mafalda@gmail.com>
2024-05-15 15:24:03 +0200

    Added first draft of full dictionary entry form TODO still missing few fields

--- src/app/controllers/editors/dictionary-entry-full-editor/dictionary-entry-full-editor.component.ts
+++ src/app/controllers/editors/dictionary-entry-full-editor/dictionary-entry-full-editor.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { DictionaryEntry } from 'src/app/models/dictionary/dictionary-entry.model';
 
 @Component({
   selector: 'app-dictionary-entry-full-editor',
   templateUrl: './dictionary-entry-full-editor.component.html',
-  styleUrls: ['./dictionary-entry-full-editor.component.scss']
+  styleUrls: ['./dictionary-entry-full-editor.component.scss', '../dictionary-entry-referral-editor/dictionary-entry-referral-editor.component.scss']
 })
 export class DictionaryEntryFullEditorComponent implements OnInit {
-
-  constructor() { }
+  private readonly unsubscribe$ = new Subject();
+  statusForm: string[] = ['completed', 'reviewed', 'working'];
+  @Input() dictionaryEntry!: DictionaryEntry; //TODO set required true on Angular update
+  fullEntryForm = new FormGroup({
+    status: new FormControl<string>(''),
+    label: new FormControl<string>('', Validators.required)
+  });
+  get status() { return this.fullEntryForm.controls.status }
+  get label() { return this.fullEntryForm.controls.label }
 
   ngOnInit(): void {
+    this.status?.setValue(this.dictionaryEntry.status);
+    this.label?.setValue(this.dictionaryEntry.label);
+    
+    this.status.valueChanges.pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe(value => {
+      console.info('salvo nuovo status', value)
+      //TODO add update field
+    });
+
+    this.label.valueChanges.pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe(v => {
+      console.info('salvo nuova label', v);
+      //TODO add update field
+    })
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(null);
+    this.unsubscribe$.complete();
+  }
 }
