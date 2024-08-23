6d830bc43a70a9c92aa49c0e77ed2a16bd1346fe
Carmelo Ienco <c.ienco@xeel.tech>
2024-08-23 15:13:31 +0200

    m4.2 individuals viewer

--- src/app/controllers/viewers/ontology-individual-viewer/ontology-individual-viewer.component.ts
+++ src/app/controllers/viewers/ontology-individual-viewer/ontology-individual-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { of, take } from 'rxjs';
 import { OntologyAnnotations } from 'src/app/models/ontology/ontology-annotations.model';
+import { OntologyDescriptionInstance } from 'src/app/models/ontology/ontology-description-instance.model';
+import { OntologyIndividualDescription } from 'src/app/models/ontology/ontology-individual-description.model';
 import { CommonService } from 'src/app/services/common.service';
 
 @Component({
@@ -11,6 +13,8 @@ import { CommonService } from 'src/app/services/common.service';
 export class OntologyIndividualViewerComponent implements OnInit {
   /** onotology element data */
   public annotationsData!: Array<OntologyAnnotations>
+  /** onotology description data */
+  public descriptions!: OntologyIndividualDescription
 
   /**ontology element id */
   private id!: string;
@@ -24,6 +28,18 @@ export class OntologyIndividualViewerComponent implements OnInit {
     ).subscribe({
       next: (dataResults) => {
         this.annotationsData = dataResults;
+      },
+      error: (error) => {
+        this.commonService.throwHttpErrorAndMessage(error, `Loading data failed: ${error.error.message}`);
+      }
+    });
+
+    //FIXME usare il servizio backend
+    this.simuleGetDescriptionData(this.id).pipe(
+      take(1),
+    ).subscribe({
+      next: (descriptionResults) => {
+        this.descriptions = descriptionResults;
       },
       error: (error) => {
         this.commonService.throwHttpErrorAndMessage(error, `Loading data failed: ${error.error.message}`);
@@ -58,6 +74,35 @@ export class OntologyIndividualViewerComponent implements OnInit {
     data3.language = "it";
 
     const result = [data1, data2, data3];
+
+    return result;
+  }
+
+  //TODO ELIMINARE APPENA SARà CREATO IL VERO SERVIZIO BACKEND
+  simuleGetDescriptionData(classId: string) {
+    return of(this.retrieveDescriptionData(classId));
+  }
+
+  //TODO ELIMINARE APPENA SARà CREATO IL VERO SERVIZIO BACKEND
+  retrieveDescriptionData(classId: string): OntologyIndividualDescription {
+    let result = new OntologyIndividualDescription();
+    
+    let data1Value = new OntologyDescriptionInstance();
+    data1Value.id = "http://test/ontology/v1#i1";
+    data1Value.shortId = "i1";
+    result.types = [data1Value]
+    result.differentFrom = [data1Value];
+
+    let data41Value = new OntologyDescriptionInstance();
+    data41Value.id = "http://test/ontology/v1#i1";
+    data41Value.shortId = "i1";
+    let data42Value = new OntologyDescriptionInstance();
+    data42Value.id = "http://test/ontology/v1#lo";
+    data42Value.shortId = "lo";
+    let data43Value = new OntologyDescriptionInstance();
+    data43Value.id = "http://test/ontology/v1#la";
+    data43Value.shortId = "ia";
+    result.sameAs = [data41Value, data42Value, data43Value];
 
     return result;
   }
