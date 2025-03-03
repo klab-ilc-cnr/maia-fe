25bd890a3ccbd908f7b414ea66cf0651032cdab8
Mafalda <papini.mafalda@gmail.com>
2025-03-03 12:29:25 +0100

    fixed json parse for empty note

--- src/app/models/custom-models/dictionary-note-vocabo.ts
+++ src/app/models/custom-models/dictionary-note-vocabo.ts
@@ -5,56 +5,47 @@ export class DictionaryNoteVocabo {
         language: string;
         etymon: string;
         details: string;
-    };
-    decameronOccurrences: number;
-    frequencies: {documentId: string; frequency: number}[];
-    firstAttestation: string;
-    firstAttestationDetails: string;
-    linguisticsSemantics: string;
-    decameron: string;
-    firstAbsAttestation: string;
-    boccaccioDante: string;
-    crusche: string;
-    polyrhematics: string;
+    } = {
+            language: '',
+            etymon: '',
+            details: ''
+        };
+    decameronOccurrences: number = 0;
+    frequencies: {documentId: string; frequency: number}[] = [];
+    firstAttestation: string = '';
+    firstAttestationDetails: string = '';
+    linguisticsSemantics: string = '';
+    decameron: string = '';
+    firstAbsAttestation: string = '';
+    boccaccioDante: string = '';
+    crusche: string = '';
+    polyrhematics: string = '';
 
     constructor(note: string) {
-        try {
-            const parsedNote = JSON.parse(note);
-            this.etymology = { 
-                ...parsedNote?.etymology,
-                details: decode(parsedNote.etymology?.details) ?? ''
-             };
-            this.decameronOccurrences = parsedNote?.decameronOccurrences ?? 0;
-            this.frequencies = parsedNote?.frequencies ? [...parsedNote.frequencies] : [];
-            this.firstAttestation = parsedNote?.firstAttestation ?? '';
-            this.firstAttestationDetails = parsedNote?.firstAttestationDetails ?? '';
-            this.linguisticsSemantics = decode(parsedNote?.linguisticsSemantics) ?? '';
-            this.decameron = decode(parsedNote?.decameron) ?? '';
-            this.firstAbsAttestation = decode(parsedNote?.firstAbsAttestation) ?? '';
-            this.boccaccioDante = decode(parsedNote?.boccaccioDante) ?? '';
-            this.crusche = decode(parsedNote?.crusche) ?? '';
-            this.polyrhematics = decode(parsedNote?.polyrhematics) ?? '';
-        } catch (error) {
-            console.group('Error with note structure, note value will be ignored');
-            console.error(error);
-            console.warn('Note value: ', note);
-            console.groupEnd();
-            this.etymology = {
-                language: '',
-                etymon: '',
-                details: '',
-            };
-            this.decameronOccurrences = 0;
-            this.frequencies = [];
-            this.firstAttestation = '';
-            this.firstAttestationDetails = '';
-            this.linguisticsSemantics = '';
-            this.decameron = '';
-            this.firstAbsAttestation = '';
-            this.boccaccioDante = '';
-            this.crusche = '';
-            this.polyrhematics = '';
-        }        
+        if(note !== '') {
+            try {
+                const parsedNote = JSON.parse(note);
+                this.etymology = { 
+                    ...parsedNote?.etymology,
+                    details: decode(parsedNote.etymology?.details) ?? ''
+                 };
+                this.decameronOccurrences = parsedNote?.decameronOccurrences ?? 0;
+                this.frequencies = parsedNote?.frequencies ? [...parsedNote.frequencies] : [];
+                this.firstAttestation = parsedNote?.firstAttestation ?? '';
+                this.firstAttestationDetails = parsedNote?.firstAttestationDetails ?? '';
+                this.linguisticsSemantics = decode(parsedNote?.linguisticsSemantics) ?? '';
+                this.decameron = decode(parsedNote?.decameron) ?? '';
+                this.firstAbsAttestation = decode(parsedNote?.firstAbsAttestation) ?? '';
+                this.boccaccioDante = decode(parsedNote?.boccaccioDante) ?? '';
+                this.crusche = decode(parsedNote?.crusche) ?? '';
+                this.polyrhematics = decode(parsedNote?.polyrhematics) ?? '';
+            } catch (error) {
+                console.group('Error with note structure, note value will be ignored');
+                console.error(error);
+                console.error('Note value: ', note);
+                console.groupEnd();
+            }            
+        }
     }
 
     public noteToString(): string {
